d0d61b77950ede8104e225f12587f53da997c41b
Firaol Dugassa <firaoldugassalikassa@gmail.com>
2026-07-04 21:54:41 +0300

    update task model to be more inclusive of datas

--- lib/app/modal/task_model.dart
+++ lib/app/modal/task_model.dart
@@ -1,7 +1,9 @@
 import '../core/constants/enums/task_priority.dart';
+
 class Task {
   final String id;
-  String text;
+  String taskName;
+  String description;
   bool done;
   final DateTime createdAt;
   DateTime? completedAt;
@@ -10,19 +12,20 @@ class Task {
 
   Task({
     String? id,
-    required this.text,
+    required this.taskName,
+    required this.description,
     required this.done,
     DateTime? createdAt,
     this.completedAt,
     this.priority = TaskPriority.medium,
     this.dueDate,
-  }) : 
-    id = id ?? DateTime.now().millisecondsSinceEpoch.toString(),
-    createdAt = createdAt ?? DateTime.now();
+  }) : id = id ?? DateTime.now().millisecondsSinceEpoch.toString(),
+       createdAt = createdAt ?? DateTime.now();
 
   Map<String, dynamic> toJson() => {
     'id': id,
-    'text': text,
+    'taskName': taskName,
+    'description': description,
     'done': done,
     'createdAt': createdAt.toIso8601String(),
     'completedAt': completedAt?.toIso8601String(),
@@ -32,15 +35,14 @@ class Task {
 
   factory Task.fromJson(Map<String, dynamic> json) => Task(
     id: json['id'],
-    text: json['text'],
+    taskName: json['taskName'],
+    description: json['description'],
     done: json['done'],
     createdAt: DateTime.parse(json['createdAt']),
-    completedAt: json['completedAt'] != null 
-        ? DateTime.parse(json['completedAt']) 
+    completedAt: json['completedAt'] != null
+        ? DateTime.parse(json['completedAt'])
         : null,
     priority: TaskPriority.values[json['priority'] ?? 1],
-    dueDate: json['dueDate'] != null 
-        ? DateTime.parse(json['dueDate']) 
-        : null,
+    dueDate: json['dueDate'] != null ? DateTime.parse(json['dueDate']) : null,
   );
 }
